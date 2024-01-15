407eeca3de8d3790d9cdf1e92b5c1e174f1f9358
Your Name <you@example.com>
2024-01-15 14:05:09 -0500

    test

--- lib/entities/name_hotel_widget.dart
+++ lib/entities/name_hotel_widget.dart
@@ -1,6 +1,5 @@
-import 'package:effective_mobile_test_tasck/shared/text__sf_pro_16__widget.dart';
 import 'package:effective_mobile_test_tasck/shared/text__sf_pro_22__widget.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' ;
 
 class HotelNameWidget extends StatelessWidget {
   final String name;
